14aa49c670efd6b3e01f24bb18ed22500b70f85f
Raman Sharma <rs9463194297@gmail.com>
2021-10-16 16:46:38 +0530

    feching csv from firebase and parsing it to json

--- src/components/home/StartScreen.vue
+++ src/components/home/StartScreen.vue
@@ -8,18 +8,18 @@
     <v-row align="center" class="mt-5" justify="center">
       <LeaderBoardFilters @searchData="filterData" />
     </v-row>
+    {{ isLoading }}
+    {{ leaderBoardData }}
     <v-row align="center" class="mt-5" justify="center">
-      <LeaderBoardDataTable
-        :searchTerm="searchTerm"
-        :data="sortByRemainingTracks()"
-      />
+      <LeaderBoardDataTable :searchTerm="searchTerm" :data="leaderBoardData" />
     </v-row>
   </v-container>
 </template>
 
 <script>
 import mainData from "@/assets/data/about.json";
-import leaderBoardDataObject from "@/assets/data/leaderboardData.json";
+const { storage } = require("../../firebaseConfig");
+
 export default {
   name: "StartScreen",
   components: {
@@ -28,25 +28,77 @@ export default {
     LeaderBoardDataTable: () =>
       import("./LeaderBoardDataTable/LeaderBoardDataTable.vue"),
   },
-  data: () => ({
-    searchTerm: "",
-    mainData,
-    leaderBoardData: leaderBoardDataObject.leaderBoardData,
-    leaderBoardFilteredData: leaderBoardDataObject.leaderBoardData,
-  }),
-  mounted() {},
+  data() {
+    return {
+      searchTerm: "",
+      mainData,
+      isLoading: true,
+      leaderBoardData: [],
+    };
+  },
+  mounted() {
+    const ref = storage().ref(`test_folder`);
+
+    ref
+      .child("test_file.csv")
+      .getDownloadURL()
+      .then((url) => {
+        // `url` is the download URL
+        console.log(url);
+        // This can be downloaded directly:
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = "blob";
+        xhr.onload = function() {
+          const blob = xhr.response;
+          const myReader = new FileReader();
+          myReader.onload = (e) => {
+            const csv = e.target.result;
+            const allTextLines = csv.split(/\r\n|\n/);
+            const lines = [];
+            for (let i = 0; i < allTextLines.length; i++) {
+              const data = allTextLines[i].split(";");
+              const tarr = [];
+              for (let j = 0; j < data.length; j++) {
+                tarr.push(data[j]);
+              }
+              lines.push(tarr);
+            }
+            const allData = lines.splice(1, lines.length);
+            const studentData = [];
+            allData.forEach((data) => {
+              const dt = data[0].split(",");
+              const obj = {
+                name: dt[0],
+                badges: {
+                  track1: dt[1],
+                  track2: dt[2],
+                },
+              };
+              studentData.push(obj);
+            });
+            this.leaderBoardData = studentData;
+            this.leaderBoardFilteredData = studentData;
+            this.isLoading = false;
+          };
+
+          myReader.readAsText(blob);
+        };
+        xhr.open("GET", url);
+        xhr.send();
+      });
+  },
   methods: {
     filterData($value) {
       this.searchTerm = $value;
     },
-    sortByRemainingTracks() {
-      const data = this.leaderBoardFilteredData.sort((a, b) => {
+    sortByRemainingTracks(data) {
+      const dta = data.sort((a, b) => {
         return a.badges.track1 + a.badges.track2 <
           b.badges.track1 + b.badges.track2
           ? 0
           : -1;
       });
-      return data;
+      return dta;
     },
   },
 };
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_apiKey,
+  authDomain: process.env.VUE_APP_authDomain,
+  projectId: process.env.VUE_APP_projectId,
+  storageBucket: process.env.VUE_APP_storageBucket,
+  messagingSenderId: process.env.VUE_APP_messagingSenderId,
+  appId: process.env.VUE_APP_appId,
+  measurementId: process.env.VUE_APP_measurementId,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const storage = firebase.storage;
+
+export { storage };
